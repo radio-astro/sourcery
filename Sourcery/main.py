ef84d849d13fbeb078ccf35acbd5f42162a20eae
Sebokolodi <mll.sebokolodi@gmail.com>
2015-10-12 15:46:50 +0200

    Jason file and main

--- Sourcery/main.py
+++ Sourcery/main.py
@@ -7,7 +7,7 @@ import reliabilityestimates as rel
 import direcdepen as dd
 import os
 import datetime
-
+import json
 
 def main():
 
@@ -25,13 +25,13 @@ def main():
     add("-v","--version", action="version",version=
         "{:s} version {:s}".format(parser.prog, __version__))
    
-    add("-i", dest="image",
+    add("-i", "--image", dest="image",
         help="FITS image name")
 
-    add("-p", dest="psf", default=None,
+    add("-p", "--psfimage", dest="psf", default=None,
         help="Point Spread Function (PSF) Fits image name. Default=None")
 
-    add("-s", dest="source_finder", default="pybdsm",
+    add("-s", "--source-finder", dest="source_finder", default="pybdsm",
         help="Source finder name. Current available"
         " sourcefinders: pybdsm. Default='pybdsm'")
 
@@ -39,96 +39,101 @@ def main():
            help="PyBDSM process_image options" 
            " [may be specified multiple times]. E.g thresh_pix=1 etc.")
 
-    add("-log", dest="log_level", type=int, default=0,
+    add("-log", "--log-level",dest="log_level", type=int, default=0,
         help="Python logging module. This ranges between"
         " 0-3, where 0, 1, 3, 4 are for info, debug, error and " 
         " critical respectively. Default is 0")
 
-    add("-pref", dest="prefix", default=None, 
+    add("-pref", "-dir-prefix", dest="prefix", default=None, 
         help="Give a prefix to an output directory. Default is None")
 
-    add("-apsf", dest="add_psfcorr", action="store_true", default=False,
-        help="Do and add correlation of the sources with the PSF as "
-        " an extra source parameter for reliability estimations.  The psf"
+    add("-apsf", "--add-psfcorr", dest="add_psfcorr", action="store_true",
+        default=False, help="Do and add correlation of the sources with the"
+        "PSF as an extra source parameter for reliability estimations.  The psf"
         " name must be provided. Default is False. To set true add -apsf")
 
-    add("-alv", dest="add_locvar", action="store_true", default=False,
-        help=" Include local variance as an extra source parameter."
-        " See -apsf. Similar to -apsf.")
+    add("-alv", "--add-localvar", dest="add_locvar", action="store_true",
+        default=False, help=" Include local variance as an extra source"
+        "parameter. See -apsf. Similar to -apsf.")
 
-    add("-dmp", dest="do_relplots", action="store_false", default=True,
-        help="Make reliability density plot. Default is True."
+    add("-dmp", "--do-relplots", dest="do_relplots", action="store_false",
+        default=True, help="Make reliability density plot. Default is True."
         " To disable add -dmp on the command line.")
  
-    add("-pcr", dest="psfcorr_region", type=int, default=5, 
-        help="Data size to correlate, given in beam sizes."
+    add("-pcr", "--psfcorr-region",  dest="psfcorr_region", type=int,
+        default=5, help="Data size to correlate, given in beam sizes."
         " Default is 5 beam sizes.")
   
-    add("-lr", dest="local_region", default=10,
+    add("-lr", "--local-region", dest="local_region", default=10,
         help="Data size to compute the local variance, in beam sizes."
         " Default is 10 beam sizes.")
      
-    add("-rel_rm", dest="rel_src_excl", action="append", default=None,
-        help="Remove sources in a given region to exclude in reliability"
-        " estimations. E.g ra, dec, radius (in degrees). For more than"
+    add("-rel_rm", "--rel-sources-excl", dest="rel_src_excl", 
+        action="append", default=None, help="Remove sources in a given"
+        "region to exclude in reliability estimations."
+        "E.g ra, dec, radius (in degrees). For more than"
         " one region: ra1,dec1,radius1:ra2,dec2,radius2. Default is None.")
 
-    add("-ps", dest="pos_smooth", type=float, default=1.6, 
+    add("-ps", "--positive-smooth", dest="pos_smooth", type=float, default=1.6, 
         help="Data smoothing to eliminate noise/data peaks given by -ps * noise."
         " This is for positive side of an Fits image. Default is 1.6.")
 
-    add("-ns", dest="neg_smooth", type=float, default=0.8, 
+    add("-ns", "--negative-smooth", dest="neg_smooth", type=float, default=0.8, 
         help="This is similar to -ps above but for negative side"
         "of an image. Default is 0.8.")
 
-    add('-pisl', dest="thresh_isl", type=float, default=3,
+    add('-pisl', "--thresh-isl", dest="thresh_isl", type=float, default=3,
         help="Threshold for the island boundary in number of sigma above"
         " the mean. Determines extent of island used for fitting [pybdsm]."
         " For positive pixels. Default is 3.")
 
-    add("-ppix", dest="thresh_pix", type=float, default=5,
+    add("-ppix", "--thresh-pix", dest="thresh_pix", type=float, default=5,
         help="Source detection threshold: threshold for the island peak"
         " in number of sigma above the mean. For positive pixels."
         " Default is 5.")
  
-    add("-nisl", dest="neg_thresh_isl", type=float, default=3,
-        help="Similar to -pisl but applied to the negative pixels."
-        " Default is 3.")
+    add("-nisl", "--negthreshold-isl", dest="neg_thresh_isl",
+        type=float, default=3, help="Similar to -pisl but applied"
+        "to the negative pixels. Default is 3.")
 
-    add("-npix", dest="neg_thresh_pix", type=float, default=5,
-        help="Similar to -ppix but applied for negative pixels."
-        " Default is 5.")
+    add("-npix", "--negthreshold-pix", dest="neg_thresh_pix",
+        type=float, default=5, help="Similar to -ppix but applied"
+        "for negative pixels. Default is 5.")
 
-    add("-snr_thr", dest="snr_thresh", type=float, default=80,
-        help="Signal-to-noise threshold with reference to the minimum"
-        " source flux in an image, e.g 80* min(snr), sources with"
+    add("-snr_thr", "--snr-threshold", dest="snr_thresh", type=float,
+        default=80, help="Signal-to-noise threshold with reference"
+        "to the minimum source flux in an image, e.g 80* min(snr), sources with"
         " snr > than this are referred to high SNR sources."
         " Default is 80.")
 
-    add("-loc_thr", dest="locvar_thresh", type=float, default=0.9,
-        help="Local variance threshold. For -loc-thr of 0.9 means that"
+    add("-loc_thr", "--localvar-threshold", dest="locvar_thresh",
+        type=float, default=0.9, help="Local variance threshold."
+        "For -loc-thr of 0.9 means that"
         " sources with local variance > 0.9 * negative noise"
         " are considered sources of high local variance."
         " Default is 0.9")
 
-    add("-pc_thr", dest="psfcorr_thresh", type=float, default=0.5,
-        help="Correlation factor threshold. Sources with threshold"
-        " larger than the specified are considered sources of"
-        " high correlation. Default is 0.5.")    
+    add("-pc_thr", "--psfcorr-threshold", dest="psfcorr_thresh", 
+        type=float, default=0.5, help="Correlation factor threshold."
+        "Sources with threshold larger than the specified are"
+        "considered sources of high correlation. Default is 0.5.")    
 
-    add("-nneg", dest="num_negatives", type=float, default=8,
+    add("-nneg", "--num-negatives",dest="num_negatives", type=float, default=8,
         help="Number of negative detections around a given source."
         " If N > threshold then sources will be considered as requiring"
         " direction-dependent calibratio solutions. Default is 8.")
 
-    add("-nrgn", dest="neg_region", type=float, default=10,
+    add("-nrgn", "--neg-region", dest="neg_region", type=float, default=10,
         help="The size of a region around a sources to lookup for"
         " negatives. In Beams in sizes. Default is 10.") 
 
-    add("-nphrm", deset="phase_center_rm", type=float, default=None,
-        help="The radius from the phase center not to consider for"
-        " final direction-dependent source selection. In beam sizes."
-        " Default is None.")
+    add("-nphrm", "--phasecenter-remove", dest="phase_center_rm",
+        type=float, default=None, help="The radius from the phase center"
+        "not to consider for final direction-dependent source selection."
+        "In beam sizes. Default is None.")
+
+    add('-jc', '--json-config', dest='config',
+        help='Json config file : No default')
 
     args = parser.parse_args()
     
@@ -163,7 +168,6 @@ def main():
 
 
     # making outdirectory
-
     if args.prefix is None:
         prefix = os.path.basename(args.image).split(".")[:-1]
         prefix = ".".join(prefix)
@@ -190,9 +194,39 @@ def main():
     locregion = args.local_region
     negregion = args.neg_region
     phase_remove = args.phase_center_rm
-   
-    # reliability     
-    mc  = rel.compute(imagename=img, psfname=psf, sourcefinder_name=sf,
+  
+    
+    if args.config:
+        keys = []
+        with open(args.config) as conf:
+            jdict = json.load(conf)
+        
+        reldict = jdict["reliability"]
+        ddict = jdict["dd_tagging"]
+        sourcefin = jdict["source_finder_opts"]
+
+        for key, val in reldict.iteritems():
+            if isinstance(val, unicode):
+                reldict[key] = str(val)
+        
+        z = reldict.copy()
+        z.update(sourcefin)
+        mc = rel.compute(**z)
+        pos, neg = mc.get_reliability()
+
+        for key, val in ddict.iteritems():
+            if isinstance(val, unicode):
+                ddict[key] = str(val)
+
+        x = ddict.copy()
+        y = {"poscatalog" : pos, "negcatalog" : neg}
+        x.update(y)
+        dc = dd.Parent(**x)
+        ppos, nneg = dc.source_selection()
+        
+    else:
+        # reliability     
+        mc  = rel.compute(imagename=img, psfname=psf, sourcefinder_name=sf,
                  makeplots=mkplt, do_psf_corr=dopsf, do_local_var=doloc,
                  psf_corr_region=psfregion, local_var_region=locregion, 
                  rel_excl_src=rel_rmsrc, pos_smooth=psmth,
@@ -200,15 +234,15 @@ def main():
                  thresh_pix=pthr_pix, neg_thresh_isl=nthresh_isl,
                  neg_thresh_pix=nthresh_pix, prefix=prefix, **pybdsm_opts)
 
-    # assignign reliability values
-    pos, neg = mc.get_reliability()
+        # assignign reliability values
+        pos, neg = mc.get_reliability()
 
-    # direction dependent detection tagging
-    dc = dd.Parent(imagename=img, psfname=psf, poscatalog=pos, negcatalog=neg,
+        # direction dependent detection tagging
+        dc = dd.Parent(imagename=img, psfname=psf, poscatalog=pos, negcatalog=neg,
                    snr_thresh=sthr, local_thresh=lthr, local_region=locregion, 
                    psfcorr_region=psfregion, high_corr_thresh=pcthr,
                    negdetec_region=negregion, negatives_thresh=negthr,
                    phasecenter_excl_radius=phase_remove, prefix=prefix,
                    loglevel=log)
-    # tagging
-    ppose, nneg = dc.source_selection()
+        # tagging
+        ppose, nneg = dc.source_selection()
